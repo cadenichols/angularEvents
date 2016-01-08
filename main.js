cb7d81dfa262fd03942e1bfc79a051cdba9bcdae
Cade Nichols <cade@cadenichols.com>
2016-01-08 10:24:00 -0800

    simple emit

--- main.js
+++ main.js
@@ -2,10 +2,18 @@
 
 var app = angular.module('angularTest', []);
 
-app.controller('parentCtrl', function(){
+app.controller('parentCtrl', function($scope){
+  $scope.message = '____';
+  $scope.$on('newMessage', function(event, args){
+    console.log('event:', event);
+    console.log('args:', args);
 
+  });
 });
 
-app.controller('childCtrl', function(){
-
+app.controller('childCtrl', function($scope){
+  $scope.click = function() {
+    $scope.$emit('newMessage', 'whatever...');
+    console.log('emit!');
+  };
 });
